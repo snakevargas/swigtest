058d9d994c37db26f7688314c601173e825cc1fe
Amit <amit.joshi@moovweb.com>
2013-09-11 12:30:51 -0700

    Making build script work with the nesting stuff

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ if [[ ( -n $1 ) && ( $1 == "clean" ) ]]; then
 fi
 
 swig -go -intgosize 64 -c++ thingy.i
-g++ -c -fpic thingy.C
+g++ -c -fpic thingy.C subthingy.C
 g++ -c -fpic thingy_wrap.cxx
-g++ -shared thingy.o thingy_wrap.o -o thingy.so
+g++ -shared thingy.o thingy_wrap.o subthingy.o -o thingy.so
 /usr/local/go/pkg/tool/darwin_amd64/6g thingy.go
 /usr/local/go/pkg/tool/darwin_amd64/6c -I /usr/local/go/pkg/darwin_amd64/ thingy_gc.c
 /usr/local/go/pkg/tool/darwin_amd64/pack grc thingy.a thingy.6 thingy_gc.6
